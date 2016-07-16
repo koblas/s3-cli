d4511491948fc0b2405ef028e11831bdf4b4170c
David Koblas <david@tubularlabs.com>
2016-07-16 16:51:00 -0400

    make the directory

--- copy.go
+++ copy.go
@@ -53,6 +53,10 @@ func copyToLocal(config *Config, src, dst *FileURI, ensure_directory bool) error
     }
 
     if ensure_directory {
+        dir := filepath.Dir(src.Path)
+        if _, err := os.Stat(dir); err != nil {
+            os.Mkdirall(dir)
+        }
     }
 
     fd, err := os.Create(dst_path)
